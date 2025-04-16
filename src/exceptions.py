94da9a9df79b42412f214329e936fcf1bcfbf6cc
Santiago Salomon <s.salomon@alumno.um.edu.ar>
2025-04-16 11:59:51 -0300

    Implementada excepción NumeroDebeSerPositivo y ValueError

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,5 +1,9 @@
 class ValueError(Exception):
-    pass
+    def __init__(self, message="La entrada debe ser un número válido"):
+        self.message = message
+        super().__init__(self.message)
 
 class NumeroDebeSerPositivo(Exception):
-    pass
+    def __init__(self, message="El número debe ser positivo"):
+        self.message = message
+        super().__init__(self.message)
